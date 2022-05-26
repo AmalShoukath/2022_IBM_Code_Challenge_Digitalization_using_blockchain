f6e755c96e46604769476d50d9011302a7a2365f
Amal-Abdul-Azeez <53385671+Amal-Abdul-Azeez@users.noreply.github.com>
2022-05-27 04:20:24 +0530

    Blockchain code

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,8 @@
 import datetime
 import hashlib
-import JSON
-from flask import Flask, jsonify
+import flask 
+from flask import Flask
+from flask import json
 class Block:
   def __init__(self, index,timestamp,data,previousHash=""):
     self.index = index
@@ -11,24 +12,31 @@ class Block:
     self.hash=self.calculateHash()
 
   def calculateHash(self):
-      return hashlib.sha256(self.index+self.previousHash+self.timestamp+JSON.stringify(self.data)).hexdigest()
+        string= str(self.index)+str(self.previousHash)+str(self.timestamp)+ str(json.dumps(self.data))
+        return hashlib.sha256(string.encode()).hexdigest()
 
 class Blockchain:
     def __init__(self):
         self.chain=[self.createGenesisBlock()]
-    def createGenesisBlock():
+    def createGenesisBlock(self):
         return Block(0,"26/05/2022","Genesis block","0")
 
     def getLatestBlock(self):
-        return self.chain[self.chain.length-1]
+        return self.chain[len(self.chain)-1]
     
-    def addBlock(newBlock):
+    def addBlock(self,newBlock):
         newBlock.previousHash=self.getLatestBlock().hash
-        newBlock.hash=newBlock.calculateHash
-        self.chain.push(newBlock)
+        newBlock.hash=newBlock.calculateHash()
+        self.chain.append(newBlock)
+    def __str__(self) -> str:
+        return self.data
         
 plot1 = Blockchain()
-plot1.addBlock(Block(1,))
-
+B1=Block(1,"23/05/2022",{"amount": 4})
+plot1.addBlock(B1)
+B2=Block(1,"24/05/2022",{"amount": 10})
+plot1.addBlock(B2)
+for items in plot1.chain:
+    print(str(items.data))
 
 
