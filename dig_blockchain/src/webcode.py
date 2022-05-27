4a41398903418e472fdb8ce53f427a7de82175b2
AmalShoukath <58292492+AmalShoukath@users.noreply.github.com>
2022-05-27 15:26:40 +0530

    Changes done

--- dig_blockchain/src/webcode.py
+++ dig_blockchain/src/webcode.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/admin/')
+
+@app.route('/admin')
 def admin():
     return render_template('admin.html')
+
+@app.route('/properties')
+def list_properties():
+    if (request.method == "GET"):
+        return jsonify({"message":"Hello World"})
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
