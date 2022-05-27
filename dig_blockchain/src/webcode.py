955d4480361cd550a9f34071f9fa5b6e8f50921a
Amal-Abdul-Azeez <53385671+Amal-Abdul-Azeez@users.noreply.github.com>
2022-05-27 16:25:29 +0530

    Update webcode.py

--- dig_blockchain/src/webcode.py
+++ dig_blockchain/src/webcode.py
@@ -1,5 +1,5 @@
 import sys
-
+from blockchain import Block,Blockchain
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
@@ -16,12 +16,16 @@ def list_properties():
     if (request.method == "GET"):
         return jsonify({"message":"Hello World"})
 
-@app.route('/chain',methods=['GET'])
-def get_chain():
-   chain_data=[]
-   for block in plot1.chain:
-       chain_data.append(str(block.data))
-   return chain_data
+@app.route('/show_land_trans', methods=['GET'])
+def mine():
+    plot1 = Blockchain()
+    B1=Block(1,"23/05/2022",{"amount": 4})
+    plot1.addBlock(B1)
+    B2=Block(1,"24/05/2022",{"amount": 10})
+    plot1.addBlock(B2)
+    for items in plot1.chain:
+    return jsonify(items)
+    
 
 
 if __name__ == '__main__':
