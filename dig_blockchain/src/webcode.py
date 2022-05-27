c7f265f8d1f376c276bb4fddc98a01fa9b6acf0e
AmalShoukath <58292492+AmalShoukath@users.noreply.github.com>
2022-05-27 11:12:28 +0530

    Some changes made in webcode

--- dig_blockchain/src/webcode.py
+++ dig_blockchain/src/webcode.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,36 +8,4 @@ def home():
 if __name__ == '__main__':
    app.run()
 
-@app.route('/adminlogin')
-def adminlogin():
-    return render_template("adminlogin.html")
 
-@app.route('/userlogin')
-def userlogin():
-    return render_template("userlogin.html")
-
-@app.route('/admincode',methods=['post'])
-def admincode():
-    username=request.form['textfield']
-    password=request.form['textfield2']
-    qry="select * from adminlogin where user_name=%s and password=%s"
-    val=(username,password)
-    res=selectone(qry,val)
-    if res is None:
-        return '''<script> alert("Invalid User");window.location="/"; </script>'''
-    elif res[3]=="admin":
-        session['lid']=res[0]
-        return '''<script> alert("Admin User"); window.location="/admin_home"; </script> '''
-
-@app.route('/usercode',methods=['post'])
-def usercode():
-    username=request.form['textfield']
-    password=request.form['textfield2']
-    qry="select * from userlogin where user_name=%s and password=%s"
-    val=(username,password)
-    res=selectone(qry,val)
-    if res is None:
-        return '''<script> alert("Invalid User");window.location="/"; </script>'''
-    elif res[3]=="admin":
-        session['lid']=res[0]
-        return '''<script> alert("Admin User"); window.location="/user_home"; </script> '''
